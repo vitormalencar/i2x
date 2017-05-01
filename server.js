5137921c006d557378cfa76eecea63666352ef09
vitormalencar <vitor.maia.alencar@gmail.com>
2017-05-01 15:52:05 -0300

    enable serve redirect

--- server.js
+++ server.js
@@ -6,4 +6,11 @@ const PORT = process.env.PORT || 8080;
 // this is to serve the frontend part
 app.use(express.static('dist/'));
 
+app.use('/*', function(req, res){
+  if(req.headers['x-forwarded-proto']!=='https'){
+    res.redirect(301, 'https://ix2-challenge.herokuapp.com/'+req.url);
+  }
+  res.sendFile(__dirname + '/index.html');
+});
+
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
