05266f79a4da75fbf0ce9a53e6cd5b1b28bc07a0
vitormalencar <vitor.maia.alencar@gmail.com>
2017-05-01 14:47:45 -0300

    add util authentication file

---
+++ src/utils/auth.js
@@ -0,0 +1,27 @@
+/**
+   * Authenticate a user. Save a token string in Local Storage
+   *
+   * @param {string} token
+*/
+export const authenticateUser = (token) => localStorage.setItem('token', token);
+
+/**
+   * Check if a user is authenticated - check if a token is saved in Local Storage
+   *
+   * @returns {boolean}
+*/
+export const isUserAuthenticated = () => localStorage.getItem('token') !== null;
+
+/**
+   * Deauthenticate a user. Remove a token from Local Storage.
+   *
+*/
+export const deauthenticateUser = () => localStorage.removeItem('token');
+
+/**
+   * Get a token value.
+   *
+   * @returns {string}
+*/
+
+export const getToken = () => localStorage.getItem('token') || '';
