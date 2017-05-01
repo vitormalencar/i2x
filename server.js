a88337f668ba90cd61f1c10e26ab303c735985e0
vitormalencar <vitor.maia.alencar@gmail.com>
2017-05-01 17:09:24 -0300

    revert redirect

--- server.js
+++ server.js
@@ -6,17 +6,11 @@ const PORT = process.env.PORT || 8080;
 // this is to serve the frontend part
 app.use(express.static('dist/'));
 
-app.use((req, res, next) => {
-  let newURL;
-  // If not on HTTPS, or not on the main domain, redirect
-  if (process.env.NODE_ENV === 'production' &&
-    (req.headers['x-forwarded-proto'] !== 'https' || req.headers.host !== 'ix2-challenge.herokuapp.com/')) {
-
-    newURL = ['https://ix2-challenge.herokuapp.com/', req.url].join('');
-    return res.redirect(newURL);
-  }
-
-  return next();
+app.use('/*', (req, res) => {
+	if (req.headers['x-forwarded-proto'] !== 'https') {
+		res.redirect(301, 'https://ix2-challenge.herokuapp.com/' + req.url);
+	}
+	res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
