0ab3e0fb76b99622d7f79f4ff7d696a206dc6b6c
vitormalencar <vitor.maia.alencar@gmail.com>
2017-05-01 17:06:09 -0300

    try redirect on root

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ const PORT = process.env.PORT || 8080;
 // this is to serve the frontend part
 app.use(express.static('dist/'));
 
-app.use('/*', function(req, res){
-  if(req.headers['x-forwarded-proto']!=='https'){
-    res.redirect(301, 'https://ix2-challenge.herokuapp.com/'+req.url);
+app.use((req, res, next) => {
+  let newURL;
+  // If not on HTTPS, or not on the main domain, redirect
+  if (process.env.NODE_ENV === 'production' &&
+    (req.headers['x-forwarded-proto'] !== 'https' || req.headers.host !== 'ix2-challenge.herokuapp.com/')) {
+
+    newURL = ['https://ix2-challenge.herokuapp.com/', req.url].join('');
+    return res.redirect(newURL);
   }
-  res.sendFile(__dirname + '/index.html');
+
+  return next();
 });
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
